d9d6c4d75d83ac8b4841a2a8034b787e9226883e
lakshay123-ctrl <lakshay.7455@varuna.sunbeamschools.com>
2020-10-03 19:42:00 +0530

    made two constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,chain1,conlog,conlog1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +34,14 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    conlog = new Log(130,130,100,PI/2);
+    conlog1 = new Log(140,140,80,PI/2);
 
     bird = new Bird(100,100);
+
+    chain1 = new Chain(bird.body,conlog.body);
+
+    chain2 = new Chain(conlog.body,conlog1.body);
 
 }
 
@@ -49,6 +56,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
+    conlog1.display();
 
     box3.display();
     box4.display();
@@ -61,4 +69,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    conlog.display();
+    chain1.display();
+    chain2.display();
 }
